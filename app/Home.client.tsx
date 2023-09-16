c778a60fecac03951878a26652a953836d3328e3
Dhanesh Phulphagar <41522314+darayn@users.noreply.github.com>
2023-09-16 20:36:48 +0530

    remove bolier from next

---
+++ app/Home.client.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div className="w-full max-w-2xl p-4 rounded shadow-lg">
+        <h1 className="text-6xl font-bold mb-4 text-center">
+          Know your course
+        </h1>
+        <p className="text-2xl text-center">
+          This web-app will help students/professionals choose courses wisely,
+          and allow course moderators to get constructive reviews/feedback.
+        </p>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      This is course feedback platform
-    </main>
-  );
+import Home from "./Home.client";
+
+export default function Page() {
+  return <Home />;
 }
